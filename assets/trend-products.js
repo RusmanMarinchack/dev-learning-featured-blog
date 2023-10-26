1d443f8cb6f6ab8fb52c1288cf9036cbf6893f77
Ruslan <ruslan2098007@gmail.com>
2023-10-26 17:30:48 +0300

    finish trend-products

--- assets/trend-products.js
+++ assets/trend-products.js
@@ -4,11 +4,17 @@ document.addEventListener('DOMContentLoaded', () => {
     if(trendProducts.length >= 1) {
         trendProducts.forEach(section => {
             const sectionId = section.getAttribute('id');
-            let mql = window.matchMedia("(max-width: 767px)");
+            let mql = window.matchMedia("(max-width: 767.98px)");
             let trendProductsSlider;
+
             function sliderInit() {
                 trendProductsSlider = new Swiper(`#${sectionId} .js-trend-products-slider`, {
-                    spaceBetween: 20,
+                    slidesPerView: 1,
+                    spaceBetween: 16,
+                    pagination: {
+                        el: '.trend-products__swiper-pagination',
+                        clickable: true
+                    },
 
                 })
             }
@@ -36,6 +42,7 @@ document.addEventListener('DOMContentLoaded', () => {
                 if (spots.length >= 1) {
                     spots.forEach(spot => {
                         spot.addEventListener('click', function () {
+
                             const popup = this.closest('.trend-products__box-spot').querySelector('.trend-products__popup');
 
                             if (this.classList.contains('spot-active')) {
@@ -64,31 +71,44 @@ document.addEventListener('DOMContentLoaded', () => {
 
             // Read the spot position so that the popup does not go beyond the frame.
             function  handlerPositionSpots() {
-                const trendProductsItem = document.querySelectorAll('.trend-products__item')
+                const trendProductsItem = document.querySelectorAll(`#${sectionId} .trend-products__item`);
 
                 if (trendProductsItem.length >= 1) {
-                    trendProductsItem.forEach(item => {
-                        const itemHeight = item.clientHeight;
-                        const itemWidth = item.clientWidth;
-                        const spots = item.querySelectorAll('.trend-products__box-spot');
+                    trendProductsItem.forEach((item, index) => {
+                        const boxSpots = item.querySelectorAll('.trend-products__box-spot');
 
-                        spots.forEach((spot) => {
-                            const popup = spot.querySelector('.trend-products__popup');
+                        boxSpots.forEach((box) => {
+                            const spot = box.querySelector('.trend-products__btn-plus');
+                            const popup = box.querySelector('.trend-products__popup');
                             const popupHeight = popup.clientHeight;
                             const spotTop = spot.offsetTop;
-                            // const spotLeft = spot.offsetLeft
+                            const spotLeft = spot.offsetLeft;
 
                             if (spotTop <= popupHeight) {
-                                popup.classList.add('popup-down')
+                                popup.style.top = `${spotTop + spot.clientHeight + 4}px`;
                             } else {
-                                popup.classList.remove('popup-down')
+                                popup.style.top = `${spotTop - popupHeight - 4}px`;
                             }
-                        })
-                    })
+
+                            if (window.innerWidth > 767.98) {
+                                popup.style.left = `${spotLeft + spot.clientWidth}px`;
+                            } else {
+                                popup.style.left = `0px`;
+                            }
+
+                            if ((index + 1) % 4 === 0 && window.innerWidth > 1279.98 ||
+                                (index + 1) % 3 === 0 && window.innerWidth > 991.98 && window.innerWidth < 1279.98 ||
+                                (index + 1) % 2 === 0 && window.innerWidth > 767.98 && window.innerWidth < 991.98) {
+                                popup.style.left = `${spotLeft - popup.clientWidth - 4}px`;
+                            }
+
+                        });
+                    });
                 }
             }
-            handlerPositionSpots();
 
+            handlerPositionSpots();
+            window.addEventListener('resize', handlerPositionSpots);
         });
     }
 });
