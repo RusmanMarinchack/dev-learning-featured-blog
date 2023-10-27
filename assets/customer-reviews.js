d8aeebff1be524de469a052e0c32f094d4f10933
Ruslan <ruslan2098007@gmail.com>
2023-10-27 17:48:13 +0300

    add reviews

---
+++ assets/customer-reviews.js
@@ -0,0 +1,36 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const customerReviews = document.querySelectorAll('.customer-reviews');
+
+    if (customerReviews.length >= 1) {
+        customerReviews.forEach(section => {
+            const sectionId = section.getAttribute('id');
+
+            const customerReviewsSlider = new Swiper(`#${sectionId} .js-customer-reviews-slider`, {
+                slidesPerView: 1,
+                spaceBetween: 16,
+                pagination: {
+                    el: '.customer-reviews__swiper-pagination',
+                    clickable: true
+                },
+                breakpoints: {
+                    991.98: {
+                        slidesPerView: 1.5,
+                    },
+                    1279.98: {
+                        slidesPerView: 2,
+                    }
+                },
+                on: {
+                    init: function () {
+                        if (this.slides.length <= 1) {
+                            this.params.spaceBetween = 0
+                        }
+                    }
+                }
+            })
+
+
+
+        })
+    }
+});
