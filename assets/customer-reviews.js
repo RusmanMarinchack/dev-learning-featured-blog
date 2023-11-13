61495bbaca7d25e480c104909b8f160a9facd540
Ruslan <ruslan2098007@gmail.com>
2023-11-13 14:24:59 +0200

    finish reviews

--- assets/customer-reviews.js
+++ assets/customer-reviews.js
@@ -4,6 +4,26 @@ document.addEventListener('DOMContentLoaded', () => {
     if (customerReviews.length >= 1) {
         customerReviews.forEach(section => {
             const sectionId = section.getAttribute('id');
+            const customerReviewsContent = document.querySelector(`#${sectionId} .customer-reviews__content`);
+            const lengthSlideDesktop = customerReviewsContent.dataset.lengthSlide;
+            let minMac;
+            let table;
+            let mobile;
+
+            // Depending on the choice of settings "Reviews layout", we display the number of slides when adaptive.
+            if (lengthSlideDesktop === '3') {
+                minMac = 3;
+                table = 3;
+                mobile = 2;
+            } else if (lengthSlideDesktop === '2') {
+                minMac = 2;
+                table = 1.5;
+                mobile = 1;
+            } else {
+                minMac = 1;
+                table = 1;
+                mobile = 1;
+            }
 
             const customerReviewsSlider = new Swiper(`#${sectionId} .js-customer-reviews-slider`, {
                 slidesPerView: 1,
@@ -13,11 +33,14 @@ document.addEventListener('DOMContentLoaded', () => {
                     clickable: true
                 },
                 breakpoints: {
+                    767.98: {
+                        slidesPerView: mobile,
+                    },
                     991.98: {
-                        slidesPerView: 1.5,
+                        slidesPerView: table,
                     },
                     1279.98: {
-                        slidesPerView: 2,
+                        slidesPerView: minMac,
                     }
                 },
                 on: {
